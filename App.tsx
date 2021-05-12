d26f1233f880566e0d57792cffe0991a208ad9cf
mike connor <mike@rocketnumber9.org>
2021-05-12 18:01:53 -0500

    run fix

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import Animated, {
   withTiming,
   useAnimatedStyle,
   Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
-import React from "react";
+} from 'react-native-reanimated';
+import { View, Button } from 'react-native';
+import React from 'react';
 
 export default function AnimatedStyleUpdateExample(props) {
   const randomWidth = useSharedValue(10);
@@ -25,14 +25,14 @@ export default function AnimatedStyleUpdateExample(props) {
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
       }}
     >
       <Animated.View
         style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
+          { width: 100, height: 80, backgroundColor: 'black', margin: 30 },
           style,
         ]}
       />
