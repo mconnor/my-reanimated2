d25bf2931407089234d2e914ddb481e34ff0a10c
mike connor <mike@rocketnumber9.org>
2021-05-14 10:22:05 -0500

    tsx

--- pages/_document.js
+++
@@ -1,2 +0,0 @@
-// @generated: @expo/next-adapter@2.1.71
-export { default } from '@expo/next-adapter/document';
---
+++ pages/_document.tsx
@@ -0,0 +1,2 @@
+// @generated: @expo/next-adapter@2.1.71
+export { default } from '@expo/next-adapter/document'
